c1c47dc52387f6a8f406e226eb8e1f1eb33f3395
visiky <736929286@qq.com>
2021-02-18 21:27:12 +0800

    Updates

---
+++ component---src-pages-404-tsx-6254276ac88b4d60ca30.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("yDyU"),u=n.n(a),o=n("N5vR");t.default=function(){return u.a.createElement(o.a,null,u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-6254276ac88b4d60ca30.js.map
